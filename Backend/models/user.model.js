e1290567d245adf534379ddcfee034b73918deab
hsraval <hsraval1998@gmail.com>
2026-03-28 12:13:46 +0530

    Auth_feature

---
+++ Backend/models/user.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+      maxlength: [100, 'Name too long'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, 'Invalid email format'],
+    },
+    passwordHash: { 
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'moderator', 'admin'],
+      default: 'user',
+    },
+    deleted:{
+      type:Boolean,
+      default:false
+    },
+    isActive:{
+      type:Boolean,
+      default:true
+    },
+    lastLoginAt:{
+      type:Date
+    },
+    resetPasswordToken:String,
+    resetPasswordExpire:Date
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('user', userSchema);
