7522d17418adcfe861e0b5a531e8cd5ca6c180f4
Kashyapgaliya29 <kashyapgaliya295@gmail.com>
2026-03-31 13:12:47 +0530

    Testing Order Modele

--- Backend/routes/order.router.js
+++ Backend/routes/order.router.js
@@ -13,17 +13,17 @@ const {
   cancleOrder
 } = require("../controllers/order.controller");
 
-// const protect = require("../middleware/auth.middleware");
+const protect = require("../middleware/auth.middleware");
 // const authorizeRoles = require("../middleware/role.middleware");
 
-router.post("/", createOrder); // user can create order
-router.get("/user",getUserOrders); // specific use can see his orders
-router.get("/:id",getOrderById); // user can see specific order
-router.get("/",getAllOrders); // admin can see all orders
-router.patch("/:id/status",updateOrderStatus); // admin can update status
-router.delete("/:id",deleteOrder); // user or admin can delete orders
-router.patch("/:id/restore",restoreOrder); // admin can restore order
-router.patch("/:id",updateOrderDetail) // user update order detail only before pending
-router.post(':/id',cancleOrder) // user can cancle order before pending
+router.post("/",protect,createOrder); // user can create order
+router.get("/user",protect,getUserOrders); // specific use can see his orders
+router.get("/:id",protect,getOrderById); // user can see specific order
+router.get("/",protect,getAllOrders); // admin can see all orders
+router.patch("/:id/status",protect,updateOrderStatus); // admin can update status
+router.delete("/:id",protect,deleteOrder); // admin can delete orders
+router.patch("/:id/restore",protect,restoreOrder); // admin can restore order
+router.patch("/:id",protect,updateOrderDetail) // user update order detail only before pending
+router.post('/:id',protect,cancleOrder) // user can cancle order before pending
 
 module.exports = router;
