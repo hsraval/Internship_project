c8b933deb441861c6d76ae1ed71b106ec7a31948
hsraval <hsraval1998@gmail.com>
2026-04-23 17:25:39 +0530

    changes

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -105,9 +105,10 @@ exports.login= async(req,res) =>{
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"strict",
-            maxAge:24*60*60*1000
+            secure:true,
+            sameSite:"None",
+            maxAge:24*60*60*1000,
+            path:"/"
         });
 
         res.status(200).json({
